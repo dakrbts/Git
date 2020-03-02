7e24e83036094a87cf5c2fd5ec02db6ee822ad48
dakrbts <dakrbts@gmail.com>
2020-03-01 23:54:38 -0500

    update Logout.java

--- src/test/Logout.java
+++ src/test/Logout.java
@@ -3,5 +3,12 @@ package test;
 public class Logout {
     public static void main(String[] args) {
         System.out.println("GoodBey World");
+
+        //updated new code
+
+
+        //hi
+
+        System.out.println();
     }
 }
